c4c4985503f9ba8356eb068df723869af4e9e049
abdulhalim <freelancerabdulhalim1997@gmail.com>
2025-01-13 17:06:46 +0600

    web app update 13/01/2025

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart';
 import 'package:world_bank_loan/auth/LoginScreen.dart';
 import 'package:world_bank_loan/auth/SignupScreen.dart';
+
+import 'dart:html' as html;
 
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("World Bank Developemnet"),
+        title: Text("World Bank Development"),
         centerTitle: true,
       ),
       body: Center(
         child: Container(
-          width: MediaQuery.of(context).size.width < 600 ? double.infinity : 600,
+          width:
+              MediaQuery.of(context).size.width < 600 ? double.infinity : 600,
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
           color: Colors.white,
           child: Column(
@@ -35,10 +38,10 @@ class LandingPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 20),
-
               ElevatedButton(
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>SignupScreen()));
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SignupScreen()));
                 },
                 child: Text('Registration'),
                 style: ElevatedButton.styleFrom(
@@ -48,30 +51,18 @@ class LandingPage extends StatelessWidget {
               SizedBox(height: 10),
               ElevatedButton(
                 onPressed: () {
-
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
-
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
                 },
                 child: Text('Login'),
                 style: ElevatedButton.styleFrom(
                   minimumSize: Size(double.infinity, 50),
                 ),
               ),
-
               SizedBox(height: 10),
               ElevatedButton(
-                onPressed: () async {
-                  const url = 'https://wbli.org/storage/app/world_bank_loan.apk';
-                  if (await canLaunchUrl(Uri.parse(url))) {
-                    await launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication);
-                  } else {
-                    // যদি URL খুলতে ব্যর্থ হয়, একটি বার্তা দেখান
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('Could not launch $url'),
-                      ),
-                    );
-                  }
+                onPressed: () {
+                  showDownloadDialog(context);
                 },
                 child: Text("Download App"),
                 style: ElevatedButton.styleFrom(
@@ -79,10 +70,83 @@ class LandingPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 10),
-
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  void showDownloadDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return DownloadProgressDialog(
+          onDownloadComplete: () {
+            Navigator.pop(context);
+            showInstallDialog(context);
+          },
+        );
+      },
+    );
+  }
+
+  void showInstallDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Download Complete'),
+          content: Text('Would you like to install the app?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                downloadFile(
+                  "https://wbli.org/storage/app/world_bank_loan.apk",
+                  "world_bank_loan.apk",
+                );
+              },
+              child: Text('Install'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void downloadFile(String url, String fileName) {
+    final html.AnchorElement anchor = html.AnchorElement(href: url)
+      ..target = '_blank'
+      ..download = fileName;
+    anchor.click();
+  }
+}
+
+class DownloadProgressDialog extends StatelessWidget {
+  final VoidCallback onDownloadComplete;
+
+  DownloadProgressDialog({required this.onDownloadComplete});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 3), onDownloadComplete);
+
+    return AlertDialog(
+      title: Text('Downloading'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text('Please wait...'),
+          SizedBox(height: 10),
+          LinearProgressIndicator(),
+        ],
       ),
     );
   }
